b74dbb6d210028376b5ac68799adc868d97863c6
Saamer Mansoor <i@saamer.me>
2025-06-03 00:38:00 -0400

    Fixed gooddoctor

--- stream44313/script.js
+++ stream44313/script.js
@@ -392,8 +392,10 @@ function showRightTranscript() {
     languageCode = response.outputLanguage3; // Use output3 for translationNumber 3
   } else if (translationNumberParam == 4) {
     languageCode = response.outputLanguage4; // Use output4 for translationNumber 4
-  } else {
+  } else if (translationNumberParam == 0) {
     languageCode = response.inputLanguage; // Default to input language
+  } else{
+    // Don't do anything
   }
   if (languageCode === response.inputLanguage) {
     currentTranscriptText = response.input;
