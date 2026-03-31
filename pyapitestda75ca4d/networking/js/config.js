ee836e52a11523e3ab1df20298a4573d8c192299
Ubuntu <ubuntu@ip-172-31-35-166.us-east-2.compute.internal>
2026-03-31 14:26:06 +0000

    Auto add

---
+++ pyapitestda75ca4d/networking/js/config.js
@@ -0,0 +1,7 @@
+// Holds constants that don't change.
+
+export const CONFIG = {
+    API_BASE: "https://api.deafassistant.com/contact/",
+    STREAM_NAME: "pyapitestda75ca4d",
+    STORAGE_KEY: "cc_user_data"
+};
