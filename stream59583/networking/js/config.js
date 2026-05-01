84950f733772b2481fdb7de753e29d58e2ffeda7
Ubuntu <ubuntu@ip-172-31-35-166.us-east-2.compute.internal>
2026-05-01 14:52:47 +0000

    Auto add

---
+++ stream59583/networking/js/config.js
@@ -0,0 +1,7 @@
+// Holds constants that don't change.
+
+export const CONFIG = {
+    API_BASE: "https://api.deafassistant.com/contact/",
+    STREAM_NAME: "stream59583",
+    STORAGE_KEY: "cc_user_data"
+};
