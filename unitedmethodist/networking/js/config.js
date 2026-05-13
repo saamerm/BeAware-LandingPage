ca2b479a555aad8590f95693f767564f1abb04db
Ubuntu <ubuntu@ip-172-31-35-166.us-east-2.compute.internal>
2026-05-13 20:34:57 +0000

    Auto add

---
+++ unitedmethodist/networking/js/config.js
@@ -0,0 +1,7 @@
+// Holds constants that don't change.
+
+export const CONFIG = {
+    API_BASE: "https://api.deafassistant.com/contact/",
+    STREAM_NAME: "unitedmethodist",
+    STORAGE_KEY: "cc_user_data"
+};
