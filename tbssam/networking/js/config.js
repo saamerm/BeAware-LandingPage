f5bad10669f35e5c84dcb197de6f925a83fa64d2
Ubuntu <ubuntu@ip-172-31-35-166.us-east-2.compute.internal>
2026-05-28 18:25:15 +0000

    Auto add

---
+++ tbssam/networking/js/config.js
@@ -0,0 +1,7 @@
+// Holds constants that don't change.
+
+export const CONFIG = {
+    API_BASE: "https://api.deafassistant.com/contact/",
+    STREAM_NAME: "tbssam",
+    STORAGE_KEY: "cc_user_data"
+};
