9f6f92ed20f33cbd4b4d3ff2b4b26530118aed18
Ubuntu <ubuntu@ip-172-31-35-166.us-east-2.compute.internal>
2026-09-10 06:26:18 +0000

    Auto add

---
+++ generalassembly20261/networking/js/config.js
@@ -0,0 +1,7 @@
+// Holds constants that don't change.
+
+export const CONFIG = {
+    API_BASE: "https://api.deafassistant.com/contact/",
+    STREAM_NAME: "generalassembly20261",
+    STORAGE_KEY: "cc_user_data"
+};
