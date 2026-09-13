595cc1057893fae847ef13f532f59abeda7aff3c
Saamer Mansoor <i@saamer.me>
2026-09-13 10:23:23 -0400

    Remove remaining leftover pyapitest* directories from prior API testing

--- pyapitestbc47b9d0/networking/js/config.js
+++
@@ -1,7 +0,0 @@
-// Holds constants that don't change.
-
-export const CONFIG = {
-    API_BASE: "https://api.deafassistant.com/contact/",
-    STREAM_NAME: "pyapitestbc47b9d0",
-    STORAGE_KEY: "cc_user_data"
-};
--- pyapitestc210b8b4/networking/js/config.js
+++
@@ -1,7 +0,0 @@
-// Holds constants that don't change.
-
-export const CONFIG = {
-    API_BASE: "https://api.deafassistant.com/contact/",
-    STREAM_NAME: "pyapitestc210b8b4",
-    STORAGE_KEY: "cc_user_data"
-};
